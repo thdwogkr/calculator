5ada4156ea29548ebbdfbf5d7a5848c1837b2996
thdwogkr <sjhsjh1990@gmail.com>
2022-07-15 12:37:17 +0900

    CalculatorController RequestMapping -> GetMapping

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -4,14 +4,14 @@ import calculator.dto.MemberDTO;
 import calculator.session.SessionConst;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @RequiredArgsConstructor
 public class CalculatorController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String login(
             @SessionAttribute(name = SessionConst.LOGIN_USER, required = false)
             MemberDTO memberDTO) {
